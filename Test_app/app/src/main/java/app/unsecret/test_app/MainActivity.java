fcf486d683e7d7a58dc3dd039e4ebd8d8f015f4e
unsecret <github@gots.biz>
2016-07-21 19:49:50 +0200

    Update after TP String for translation with variables in it !

--- Test_app/app/src/main/java/app/unsecret/test_app/MainActivity.java
+++ Test_app/app/src/main/java/app/unsecret/test_app/MainActivity.java
@@ -1,13 +1,43 @@
 package app.unsecret.test_app;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.widget.Button;
+import android.util.Log;
+import android.widget.TextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+//public class MainActivity extends AppCompatActivity {
+
+   // @Override
+   /* protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+*/
+
+    public class MainActivity extends Activity {
+        private static final String TAG = MainActivity.class.getSimpleName();
+        Button button = null;
+        String hist = null;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_main);
+            Resources res = getResources();
+
+            String chaine = res.getString(R.string.texteGras,"1");
+            TextView vue = (TextView)findViewById(R.id.vue);
+            vue.setText(chaine);
+        }
+    }
+//}
+
+
